17a971f7935302332d496f5399cf8f8c45b5ef5c
Isaac-Y67 <ombasaisaac2005@gmail.com>
2026-06-24 13:33:01 +0300

    Delete Week8 directory

--- Week8/sms/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "<h1>Hello, World!</h1>";
-echo "<p>My name is: <strong>Ombasa Isaac</strong></p>";
-echo "<p>Project: Student Management System</p>";
-echo "<p>PHP Version running: " . phpversion() . "</p>";
-?>
